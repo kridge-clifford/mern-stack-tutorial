cd35f91866a63938ef1243a0d58844f81abe4b2c
Kridge Clifford Dela Cruz <kridge.delacruz@gmail.com>
2020-03-23 15:18:17 +0800

    initial commit

---
+++ client/src/components/post/Post.js
@@ -0,0 +1,46 @@
+import React, { Fragment, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner';
+import PostItem from '../posts/PostItem';
+import { getPost } from '../../actions/post';
+import CommentForm from './CommentForm';
+import CommentItem from './CommentItem';
+
+const Post = ({ getPost, post: { post, loading }, match }) => {
+  useEffect(() => {
+    getPost(match.params.id);
+  }, [getPost, match.params.id]);
+
+  return loading || post === null ? (
+    <Spinner></Spinner>
+  ) : (
+    <Fragment>
+      <Link to='/posts' className='btn'>
+        Back To Posts
+      </Link>
+      <PostItem post={post} showActions={false}></PostItem>
+      <CommentForm postId={post._id}></CommentForm>
+      <div className='comments'>
+        {post.comments.map(comment => (
+          <CommentItem
+            key={comment._id}
+            comment={comment}
+            postId={post._id}
+          ></CommentItem>
+        ))}
+      </div>
+    </Fragment>
+  );
+};
+
+Post.propTypes = {
+  getPost: PropTypes.func.isRequired,
+  post: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  post: state.post
+});
+export default connect(mapStateToProps, { getPost })(Post);
